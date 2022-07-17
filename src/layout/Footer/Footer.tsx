f7f272dbd2179c57ff7f9e21e142c86cd9b62b22
Aziz Bektemirov <bektemirovaziz64@yandex.com>
2022-07-17 19:07:07 +0500

    adds lint in the Footer

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -2,8 +2,6 @@ import { SiReact } from 'react-icons/si';
 import { Container } from '../../components/Container';
 
 export const Footer = (): JSX.Element => {
-    // const darkTheme = 'dark:bg-slate-900';
-
     return (
         <footer className='mt-auto bg-slate-200 pt-4'>
             <Container className='pb-4'>
@@ -12,7 +10,7 @@ export const Footer = (): JSX.Element => {
             <div className='pb-4 bg-slate-700 pt-4'>
                 <Container>
                     <p className='flex text-white justify-center text-lg items-center gap-1'>
-                        Developed with <SiReact title='React.js' style={{ color: '#61DAFB' }} className='text-2xl animate-spin-slow' /> by Bektemirov Aziz
+                        Developed with <SiReact title='React.js' style={{ color: '#61DAFB' }} className='text-2xl animate-spin-slow' /> by <a target={'_blank'} href="https://www.linkedin.com/in/aziz-bektemirov-0336181a5/" className='hover:text-blue-500 duration-200' rel="noreferrer">Bektemirov Aziz</a>
                     </p>
                 </Container>
             </div>
